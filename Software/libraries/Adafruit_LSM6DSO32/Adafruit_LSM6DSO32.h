661fab8f4d92e7396449b842a96bc8b5f368446d
Dexter Carpenter <dexter.carpenter03@gmail.com>
2026-06-10 19:20:21 +0000

    update libraries

--- Software/libraries/Adafruit_LSM6DSO32/Adafruit_LSM6DSO32.h
+++ Software/libraries/Adafruit_LSM6DSO32/Adafruit_LSM6DSO32.h
@@ -1,41 +1,49 @@
-#ifndef ADAFRUIT_LSM6DSO32_H
-#define ADAFRUIT_LSM6DSO32_H
+/*!
+ *  @file Adafruit_LSM6DSO32.h
+ *
+ * 	I2C Driver for the Adafruit LSM6DSO32 6-DoF Accelerometer and Gyroscope
+ *library
+ *
+ * 	This is a library for the Adafruit LSM6DSO32 breakout:
+ * 	https://www.adafruit.com/products/PID_HERE
+ *
+ * 	Adafruit invests time and resources providing this open source code,
+ *  please support Adafruit and open-source hardware by purchasing products from
+ * 	Adafruit!
+ *
+ *
+ *	BSD license (see license.txt)
+ */
 
-#include <Arduino.h>
-#include <Wire.h>
+#ifndef _ADAFRUIT_LSM6DSO32_H
+#define _ADAFRUIT_LSM6DSO32_H
 
-// Minimal compatibility shim for the real Adafruit_LSM6DSO32 library.
-// Provides only the types and methods used by the Meadowlark sketch so the
-// project can build inside the devcontainer without fetching upstream libs.
+#include "Adafruit_LSM6DS.h"
+#include "Adafruit_LSM6DSOX.h"
+#define LSM6DSO32_CHIP_ID 0x6C ///< LSM6DSO32 default device id from WHOAMI
 
-// Ranges / rates used by the sketch (values are irrelevant for the stub)
-#define LSM6DSO32_ACCEL_RANGE_32_G  32
-#define LSM6DS_GYRO_RANGE_2000_DPS  2000
-#define LSM6DS_RATE_208_HZ          208
+/** The accelerometer data range */
+typedef enum dso32_accel_range {
+  LSM6DSO32_ACCEL_RANGE_4_G,
+  LSM6DSO32_ACCEL_RANGE_32_G,
+  LSM6DSO32_ACCEL_RANGE_8_G,
+  LSM6DSO32_ACCEL_RANGE_16_G
+} lsm6dso32_accel_range_t;
 
-// sensors_event_t lightweight replacement used by the sketch.
-typedef struct {
-  struct { float x, y, z; } acceleration;
-  struct { float x, y, z; } gyro;
-  float temperature;
-} sensors_event_t;
-
-class Adafruit_LSM6DSO32 {
+/*!
+ *    @brief  Class that stores state and functions for interacting with
+ *            the LSM6DSO32 I2C Digital Potentiometer
+ */
+class Adafruit_LSM6DSO32 : public Adafruit_LSM6DSOX {
 public:
-  Adafruit_LSM6DSO32() {}
-  bool begin_I2C(uint8_t /*addr*/, TwoWire* /*wire*/) { return true; }
-  void setAccelRange(int /*r*/) {}
-  void setGyroRange(int /*r*/) {}
-  void setAccelDataRate(int /*r*/) {}
-  void setGyroDataRate(int /*r*/) {}
+  Adafruit_LSM6DSO32();
 
-  // Fill events with zero / nominal values so the sketch can read sensible
-  // numbers during builds/tests that don't access real hardware.
-  void getEvent(sensors_event_t* a, sensors_event_t* g, sensors_event_t* t) {
-    if (a) { a->acceleration.x = 0.0f; a->acceleration.y = 0.0f; a->acceleration.z = 9.80665f; }
-    if (g) { g->gyro.x = 0.0f; g->gyro.y = 0.0f; g->gyro.z = 0.0f; }
-    if (t) { t->temperature = 25.0f; }
-  }
+  lsm6dso32_accel_range_t getAccelRange(void);
+  void setAccelRange(lsm6dso32_accel_range_t new_range);
+  void _read(void);
+
+private:
+  bool _init(int32_t sensor_id);
 };
 
-#endif // ADAFRUIT_LSM6DSO32_H
+#endif
